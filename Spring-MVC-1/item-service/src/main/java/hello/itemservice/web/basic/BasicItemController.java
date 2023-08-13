533175a334f5c93f921624e2e559bbcbf44a02fc
nayoong02 <sonyangel02@naver.com>
2023-08-13 17:49:08 +0900

    RedirectAttributes
    상품 등록 후 `저장 완료!` 메시지를 띄워본다.

--- Spring-MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Spring-MVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -83,14 +84,23 @@ public class BasicItemController {
 //        return "basic/item";
 //    }
 
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//
+//        itemRepository.save(item);
+//
+//        // model.addAttribute("item", item);
+//
+//        return "redirect:/basic/items/" + item.getId();
+//    }
+
     @PostMapping("/add")
-    public String addItemV5(Item item) {
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
 
-        itemRepository.save(item);
-
-        // model.addAttribute("item", item);
-
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
